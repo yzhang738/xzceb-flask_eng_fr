f474d4a1a123cb5f648d6c236ecdb80a6bb57ada
yzhang738 <yzhang738@gmail.com>
2023-01-23 17:49:57 -0500

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,15 +3,15 @@ from  translator import english_to_french, french_to_english, convert_dic_to_str
 
 class Test_english_to_french(unittest.TestCase): 
     def test1(self):
-        message = "English and French translation are not equal!"
-        # en_translation = english_to_french('I')
-        # en_text = convert_dic_to_str(en_translation)
-        #self.assertEqual(convert_dic_to_str(english_to_french('Blue')), 'bleu', message)
-        self.assertEqual(convert_dic_to_str(english_to_french('Hello')), 'Bonjour', message)
-
+        message = "The translation are not equal!"
+        self.assertEqual(english_to_french('Hello'), 'Bonjour', message)
+        
 class Test_french_to_english(unittest.TestCase): 
-    def test1(self):
-        message = "English and French translation are not equal!"
-        self.assertEqual(convert_dic_to_str(french_to_english('Bonjour')), 'Hello', message)
- 
+    def test3(self):
+        message = "The translation are not equal!"
+        self.assertEqual(french_to_english('Bonjour'), 'Hello', message)
+    # def test4(self):
+    #     message = "The translation are not equal!"
+    #     self.assertNotEqual(convert_dic_to_str(french_to_english('Bleu')), 'Hello', message)
+
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,47 +1,45 @@
 """
 English to French translation using IBM Watson translator
 """
-
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
 from dotenv import load_dotenv
 load_dotenv()
+APIKEY = os.environ['apikey']
+URL = os.environ['url']
 
-apikey = os.environ['apikey']
-url = os.environ['url']
-
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator(APIKEY)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
-language_translator.set_service_url(url)
+language_translator.set_service_url(URL)
 
-def english_to_french(english_text):
+def english_to_french(text):
     """
         English to French translation function
     """ 
-    french_text = language_translator.translate(
-        text=english_text,
+    translated = language_translator.translate(
+        text=text,
         model_id='en-fr').get_result()
-    return french_text
+    return translated['translations'][0].get('translation')
 
-def french_to_english(french_text):
+def french_to_english(text):
     """
         French to English translation function
     """
-    english_text = language_translator.translate(
-        text=french_text,
+    translated = language_translator.translate(
+        text=text,
         model_id='fr-en').get_result()
-    return english_text
+    return translated['translations'][0].get('translation')
 
 def convert_dic_to_str(dict):
     """
-        Since Watson translator returns translation results in python dictionaries, we need to convert them 
-        to strings for comparision
+        Since Watson translator returns translation results in python dictionaries,  
+        we need to convert them to strings for comparision
     """
-    tran_list = dict['translations']
-    translated_text = tran_list[0].get('translation')
+    # tran_list = dict['translations']
+    translated_text = dict['translations'][0].get('translation')
     return translated_text
